60e4ccf1ce839b1ba45619da451cb0e529ccc1f9
vitaliiguziaev <vitaliiguziaev@gmail.com>
2016-12-16 18:28:16 +0300

    set ViewEncapsulation.None for AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,33 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app',
   styles: [require('bootstrap/dist/css/bootstrap.css')],
  	template: `
-<div class="container">
-  <form>
-    <div class="form-group row">
-      <label for="login" class="col-sm-1 col-form-label">Login</label>
-      <div class="col-sm-2">
-        <input type="text" class="form-control" id="login" placeholder="Login">
+  <div id="login-form" class="container">
+    <form>
+      <div class="form-group row">
+        <label for="login" class="col-sm-1 col-form-label">Login</label>
+        <div class="col-sm-2">
+          <input type="text" class="form-control" id="login" placeholder="Login">
+        </div>
       </div>
-    </div>
-    <div class="form-group row">
-      <label for="password" class="col-sm-1 col-form-label">Password</label>
-      <div class="col-sm-2">
-        <input type="password" class="form-control" id="password" placeholder="Password">
+      <div class="form-group row">
+        <label for="password" class="col-sm-1 col-form-label">Password</label>
+        <div class="col-sm-2">
+          <input type="password" class="form-control" id="password" placeholder="Password">
+        </div>
       </div>
-    </div>
-    <div class="form-group row">
-      <div class="offset-sm-2 col-sm-10">
-        <button type="submit" class="btn btn-primary">Enter</button>
+      <div class="form-group row">
+        <div class="offset-sm-2 col-sm-10">
+          <button type="submit" class="btn btn-primary">Enter</button>
+        </div>
       </div>
-    </div>
-  </form>
-</div>
-  `
+    </form>
+  </div>
+  `,
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   constructor(){
