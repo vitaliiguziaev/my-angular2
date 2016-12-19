7a234934efbb2f1d8d275081f004e1d78ef04041
vitaliiguziaev <vitaliiguziaev@gmail.com>
2016-12-19 12:06:20 +0300

    points (2-4) change css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,10 @@ import { ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app',
   styles: [require('bootstrap/dist/css/bootstrap.css')],
- 	template: `
-    <header>
-        <nav class="navbar navbar-light bg-faded">
-          <h1 class="navbar-brand mb-0">App</h1>
-        </nav>
-    </header>
-
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-    
-    <footer>
-      <span>Copyright 2016-2017</span>
-    </footer>
-  `,
+  templateUrl: './app.component.html',
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  constructor(){
+  constructor() {
   }
 }
