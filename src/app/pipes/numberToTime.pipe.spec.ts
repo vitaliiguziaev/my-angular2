40d2bb3d734afbf78b9821f5f1355f68657b3d99
VitaliiGuziaev <vitaliiguziaev@gmail.com>
2017-01-18 23:03:44 +0300

    point 10 (in progress) - pipe

--- src/app/pipes/numberToTime.pipe.spec.ts
+++ src/app/pipes/numberToTime.pipe.spec.ts
@@ -7,18 +7,22 @@ describe('Pipe - numberToTime', () => {
         numberToTimePipe = new NumberToTime();
     });
 
+    it('should 0 return as 0min', () => {
         let result = numberToTimePipe.transform(0);
         expect(result).toBe('0min');
     });
 
+    it('should 32 return as 32min', () => {
         let result = numberToTimePipe.transform(32);
         expect(result).toBe('32min');
     });
 
+    it('should 60 return as 1h 0min', () => {
         let result = numberToTimePipe.transform(60);
         expect(result).toBe('1h 0min');
     });
 
+    it('should 75 return as 1h 15min', () => {
         let result = numberToTimePipe.transform(75);
         expect(result).toBe('1h 15min');
     });
